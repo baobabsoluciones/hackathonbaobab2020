ea16ae6cabbded06c2ffd852e0ae3bcb2fb4479c
Guillermo González-Santander <guillermo.gonzalez@baobabsoluciones.es>
2020-11-27 19:45:06 +0100

    Solution proposed by GG

--- example/test_script.py
+++ example/test_script.py
@@ -1,14 +1,16 @@
-from core import Instance, Experiment
+from core import Instance
 from example.algorithm1 import Algorithm
+from example.modelo_entero_inicializacion_algoritmo import Model
 import os
 
 
 def solve_example_problem_json():
-    directory = 'data/'
-    instance_name = 'c1564_3'
+    directory = '../data/'
+    instance_name = 'c1564_9'
     path = '{}c15.mm/{}.mm'.format(directory, instance_name)
     instance = Instance.from_mm(path)
-    exp = Algorithm(instance=instance)
+    print(instance.__dict__)
+    exp = Model(instance=instance, algorithm=Algorithm(instance=instance))
     exp.solve({})
     print(exp.check_solution())
     path_solution = '{}solutions/c15.mm/{}'.format(directory, instance_name)
