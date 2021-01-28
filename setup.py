dc875035451cc853baac37be69bfd955f131a82d
pchtsp <pchtsp@gmail.com>
2021-01-28 18:37:58 +0100

    bump version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open("requirements.txt", "r") as fh:
 
 kwargs = {
     "name": "hackathonbaobab2020",
-    "version": 0.91,
+    "version": 0.93,
     "packages": packages,
     "description": "Hackathon 2020 at baobab soluciones",
     "long_description": long_description,
