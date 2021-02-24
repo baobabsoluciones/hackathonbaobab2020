aaadb9c96b4b3997bca53664b0e03511a5aea528
pchtsp <pchtsp@gmail.com>
2021-02-24 21:55:00 +0100

    fix spelling to warning.

--- hackathonbaobab2020/execution/__init__.py
+++ hackathonbaobab2020/execution/__init__.py
@@ -4,4 +4,4 @@ import warnings
 try:
     from .benchmark import *
 except ImportError:
-    warnings.warn("To use the benchmark functions, to install benchmark dependencies: \n`pip install hackathonbaobab2020[benchmark]`")
+    warnings.warn("To use the benchmark functions, you need to install the benchmark dependencies: \n`pip install hackathonbaobab2020[benchmark]`")
