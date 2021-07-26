47629f06b301a1b0d3c4b6804acbd82bccfc469c
pchtsp <pchtsp@gmail.com>
2021-07-26 19:35:00 +0200

    refactored tests

--- hackathonbaobab2020/tests/tests.py
+++ hackathonbaobab2020/tests/tests.py
@@ -6,107 +6,82 @@ sys.path.insert(1, prev_dir)
 import unittest
 import shutil
 from hackathonbaobab2020 import solve_zip, Experiment
-from hackathonbaobab2020 import solver as pkg_solvers
+
+# from hackathonbaobab2020 import solver as pkg_solvers
+# list(pkg_solvers.solvers.values())[0]
 
 
-class TestLoaderWithKwargs(unittest.TestLoader):
-    """A test loader which allows to parse keyword arguments to the
-    test case class."""
+class BaseSolverTest:
+    class HackathonTests(unittest.TestCase):
+        solver = None
 
-    def loadTestsFromTestCase(self, testCaseClass, **kwargs):
-        """Return a suite of all tests cases contained in
-        testCaseClass."""
-        if issubclass(testCaseClass, unittest.suite.TestSuite):
-            raise TypeError(
-                "Test cases should not be derived from "
-                + "TestSuite. Maybe you meant to derive from"
-                + " TestCase?"
-            )
-        testCaseNames = self.getTestCaseNames(testCaseClass)
-        if not testCaseNames and hasattr(testCaseClass, "runTest"):
-            testCaseNames = ["runTest"]
+        def setUp(self):
+            self.path_out = "data/" + self.solver + "/"
 
-        # Modification here: parse keyword arguments to testCaseClass.
-        test_cases = []
-        for test_case_name in testCaseNames:
-            test_cases.append(testCaseClass(test_case_name, **kwargs))
-        loaded_suite = self.suiteClass(test_cases)
+        def tearDown(self):
+            shutil.rmtree(self.path_out)
 
-        return loaded_suite
+        def run_scenario_instance(self, scenario, instance):
+            path_in = os.path.dirname(__file__)
+            try:
+                solve_zip(
+                    zip_name="./{}.zip".format(scenario),
+                    path_out=self.path_out,
+                    path_in=path_in,
+                    solver_name=self.solver,
+                    test=False,
+                    instances=[instance],
+                    options=dict(timeLimit=300, gapRel=1),
+                )
+                experiment = Experiment.from_json(
+                    self.path_out + scenario + "/" + instance
+                )
+                return experiment.check_solution()
+            except Exception as e:
+                raise TestFail("Test failed for solver {}: {}".format(self.solver, e))
 
+        # def test_j10(self):
+        #     return self.run_scenario_instance('j10.mm', 'j102_2.mm')
 
-class HackathonTests(unittest.TestCase):
-    def __init__(self, testName, solver, *args, **kwargs):
-        unittest.TestCase.__init__(self, testName)
-        self.solver = solver
-        self.path_out = "data/" + self.solver + "/"
+        def test_j10_2(self):
+            return self.run_scenario_instance("j10.mm", "j102_4.mm")
 
-    def tearDown(self):
-        shutil.rmtree(self.path_out)
+        def test_j10_3(self):
+            return self.run_scenario_instance("j10.mm", "j102_5.mm")
 
-    def run_scenario_instance(self, scenario, instance):
-        path_in = os.path.dirname(__file__)
-        try:
-            solve_zip(
-                zip_name="./{}.zip".format(scenario),
-                path_out=self.path_out,
-                path_in=path_in,
-                solver_name=self.solver,
-                test=False,
-                instances=[instance],
-                options=dict(timeLimit=300, gapRel=1),
-            )
-            experiment = Experiment.from_json(self.path_out + scenario + "/" + instance)
-            return experiment.check_solution()
-        except Exception as e:
-            raise TestFail("Test failed for solver {}: {}".format(self.solver, e))
+        def test_j10_4(self):
+            return self.run_scenario_instance("j10.mm", "j102_6.mm")
 
-    # def test_j10(self):
-    #     return self.run_scenario_instance('j10.mm', 'j102_2.mm')
+        # def test_c15(self):
+        #     return self.run_scenario_instance('c15.mm', 'c154_3.mm')
 
-    def test_j10_2(self):
-        return self.run_scenario_instance("j10.mm", "j102_4.mm")
-
-    def test_j10_3(self):
-        return self.run_scenario_instance("j10.mm", "j102_5.mm")
-
-    def test_j10_4(self):
-        return self.run_scenario_instance("j10.mm", "j102_6.mm")
-
-    # def test_c15(self):
-    #     return self.run_scenario_instance('c15.mm', 'c154_3.mm')
-
-    # def test_c15_3(self):
-    #     return self.run_scenario_instance('c15.mm', 'c158_3.mm')
-    #
-    # def test_c15_2(self):
-    #     return self.run_scenario_instance('c15.mm', 'c158_4.mm')
+        # def test_c15_3(self):
+        #     return self.run_scenario_instance('c15.mm', 'c158_3.mm')
+        #
+        # def test_c15_2(self):
+        #     return self.run_scenario_instance('c15.mm', 'c158_4.mm')
 
 
-def testAll():
-    runner = unittest.TextTestRunner()
-    suite_all = suite()
-    # we run all tests at the same time
-    ret = runner.run(suite_all)
-    if not ret.wasSuccessful():
-        raise RuntimeError("Tests were not passed")
+class TestAlgorithm(BaseSolverTest.HackathonTests):
+    solver = "default"
 
 
-def suite():
-    solvers = pkg_solvers.solvers.keys()
-    loader = TestLoaderWithKwargs()
-    suite = unittest.TestSuite()
-    for solver in solvers:
-        print("Testing solver {}".format(solver))
-        tests = loader.loadTestsFromTestCase(HackathonTests, solver=solver)
-        suite.addTests(tests)
-    return suite
+class TestMilp1(BaseSolverTest.HackathonTests):
+    solver = "Milp_LP_HL"
 
 
-class TestFail(Exception):
-    pass
+class TestCPModel1(BaseSolverTest.HackathonTests):
+    solver = "ortools"
+
+
+class TestIterator1(BaseSolverTest.HackathonTests):
+    solver = "Iterator_HL"
+
+
+class TestLoop_solver(BaseSolverTest.HackathonTests):
+    solver = "loop_EJ"
 
 
 if __name__ == "__main__":
-    # Tests
-    testAll()
+
+    unittest.main()
